1881c857b41aacea81ca4f08a607779809c84c0f
christopher Panerio <christopherpaneriotup@gmail.com>
2022-04-30 04:48:50 +0800

    Initial Commit

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const dotenv = require('dotenv');
 if(process.env.NODE_ENV !== 'PRODUCTION')
     require('dotenv').config({ path: 'backend/config/config.env'})
 
-dotenv.config({path: 'backend/config/config.env'});
+// dotenv.config({path: 'backend/config/config.env'});
 
 
 app.use(express.json());
